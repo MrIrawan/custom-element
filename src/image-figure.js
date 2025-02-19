2663d048fa3bf2aea6f09ac70a1f488ccb1dac22
farrel irawan <irawanssfarrel@gmail.com>
2025-02-19 15:43:10 +0700

    modifikasi pada class ImageFigure

--- src/image-figure.js
+++ src/image-figure.js
@@ -2,7 +2,16 @@ class ImageFigure extends HTMLElement {
     constructor() {
         super();
 
-        this.innerHTML = `  halo, nama saya image-figure`;
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+            <figure>
+                <img src="src/logo-g.jpg" alt="Dicoding Logo g" width="200">
+                <figcaption>Huruf g dalam logo Dicoding</figcaption>
+            </figure>
+        `;
     }
 }
 
